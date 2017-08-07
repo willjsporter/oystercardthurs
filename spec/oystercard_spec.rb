f68dc5433406f857daf6982acccfe3c0e30fdbd4
Frederick Ofori <FrederickCodes@gmail.com>
2017-08-07 14:56:31 +0100

    feature test for topup added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,9 +6,10 @@ describe OysterCard do
 		expect(subject.balance).to eq 0
 	end
 
+	it { is_expected.to respond_to(:top_up).with(1).argument }
+
 	it "should top up the card by a value" do
 		number = 2.to_f
 		expect(subject.top_up(number)).to eq "your oyster card has been topped up by £#{number.to_f}"
 		end
 	end
-
